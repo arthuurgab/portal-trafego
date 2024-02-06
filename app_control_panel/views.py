e1e4cff39dee0c510a637204c325434ec694dcfe
arthuurgab <adarocha060@gmail.com>
2024-02-06 09:44:43 -0300

    Arthur: verifica superuser and template dashboard

--- app_control_panel/views.py
+++ app_control_panel/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+@login_required
 def panel(request):
-    return render(request, "control_panel/control_panel.html")
+    if request.user.is_superuser:
+        return render(request, "control_panel/control_panel_superuser.html")
+    else:
+        return render(request, "control_panel/control_panel.html")
